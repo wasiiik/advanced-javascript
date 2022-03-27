0b41ec0962d762bc1bcf2d8484cdd102deb33267
Wasik Hasan <46458111+wasikhasan@users.noreply.github.com>
2022-03-28 00:23:04 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,20 @@
+let bonus = 20;
+
+function sum(first, second) {
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if (result > 9) {
+        let mood = "happy"
+        mood = "fishy"
+        mood = "funky"
+        mood = "cranky"
+        console.log(mood);
+    }
+    console.log(day);
+    var day = "friday"
+    return result;
+}
+
+const output = sum(3, 7);
+console.log(bonus);
+console.log(output);
