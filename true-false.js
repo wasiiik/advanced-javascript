38b1928041dc73fea1b2cb295138278fe1fd399e
Wasik Hasan <46458111+wasikhasan@users.noreply.github.com>
2022-03-27 23:15:01 +0600

    true vs false // null vs undefined

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// Falsy:
+// const name = "o";
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy
+// '0',' '
+
+
+let name = false;
+console.log(name);
+
+if (name) {
+    console.log("Condition is true");
+} else {
+    console.log("Condition is false");
+}
